ff527a7d81d17c8f909cdb6d4f0d7810149f770f
stamblerre <rls462@cornell.edu>
2014-12-30 17:34:00 -0500

    another function

--- database.py
+++ database.py
@@ -9,3 +9,17 @@ def upload_messages(db, userId, messageList):
 
         message_obj = models.Message(1, userId, direction, phoneNumber, timestamp, messageLength)
         db.session.add(message_obj) #add message to database
+    db.session.commit()
+
+def upload_contacts(db, userId, contactList):
+	for contact in contactList:
+        userId = userId
+        phoneNumber = contact["phoneNumber"]
+        contactType = contact["contactType"]
+
+        contact_obj = models.Contacts(1, userID, phoneNumber, contactType)
+      	db.session.add(contact_obj)
+    db.session.commit()
+
+def process():
+	pass
