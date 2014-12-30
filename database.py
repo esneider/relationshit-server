dd8f946afaf9f2966dbfd84b75274da961a595c6
stamblerre <rls462@cornell.edu>
2014-12-30 16:55:39 -0500

    meh

--- database.py
+++ database.py
@@ -7,5 +7,5 @@ def upload_messages(db, userId, messageList):
         timestamp = message["timestamp"]
         messageLength = message["messageLength"]
 
-        message_obj = models.Message(userId, direction, phoneNumber, timestamp, messageLength)
+        message_obj = models.Message(1, userId, direction, phoneNumber, timestamp, messageLength)
         db.session.add(message_obj) #add message to database
