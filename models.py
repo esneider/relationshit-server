8488d6cbe1cc8130b8dd7041c34d814a149c73de
stamblerre <rls462@cornell.edu>
2014-12-30 16:54:58 -0500

    back to primary keys

--- models.py
+++ models.py
@@ -6,13 +6,15 @@ db = SQLAlchemy()
 class Message(db.Model):
     __tablename__ = 'Messages'
 
+    messageId = db.Column(db.Integer, nullable = False, primary_key = True)
     userId = db.Column(db.Integer, nullable = False)     # add foreign key
     direction = db.Column(db.String(80), nullable = False)     # either 'send' or 'receive'
     phoneNumber = db.Column(db.String(120), nullable = False)
     timestamp = db.Column(db.Integer, nullable = False)
     messageLength = db.Column(db.Integer, nullable = False)
 
-    def __init__(self, userId, direction, phoneNumber, timestamp, messageLength):
+    def __init__(self, messageId, userId, direction, phoneNumber, timestamp, messageLength):
+        self.messageId = messageId
         self.userId = userId
         self.direction = direction
         self.phoneNumber = phoneNumber
@@ -22,11 +24,13 @@ class Message(db.Model):
 class Contacts(db.Model):
     __tablename__ = 'Contacts'
 
+    contactId = db.Column(db.Integer, nullable = False, primary_key = True)
     userId = db.Column(db.Integer, nullable = False)
     phoneNumber = db.Column(db.String(120), nullable = False)
     contactType = db.Column(db.String(120), nullable = False)     # default is "regular"
 
-    def __init__(self, userId, phoneNumber, contactType):
+    def __init__(self, contactId, userId, phoneNumber, contactType):
+        self.contactId = contactId
         self.userId = userId
         self.phoneNumber = phoneNumber
         self.contactType = contactType
