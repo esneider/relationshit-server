c7919450dde5647cd721d3f75f39e1aa45802e9a
Miriam Huntley <mhuntley@fas.harvard.edu>
2014-12-30 14:13:12 +0200

    added db stuff to hello

--- hello.py
+++ hello.py
@@ -2,6 +2,9 @@ import os
 from flask import Flask, request, json
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
+
 
 @app.route('/')
 def hello():
