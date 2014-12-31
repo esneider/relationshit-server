c487b38719e38ae15af2c4ed13cbae96d72b9b4f
stamblerre <rls462@cornell.edu>
2014-12-31 01:45:31 -0500

    plz

--- database.py
+++ database.py
@@ -87,7 +87,7 @@ def populate_contacts(db, user_id):
 
     print "before first query"
     try_this = db.session.query(models.Message).filter_by(userId=user_id)
-    print try_this
+    print "hi" + str(try_this)
     #all_sent_contacts = db.session.query(models.Message.phoneNumber, db.func.count(models.Message.phoneNumber).label("sent_count")).filter_by(userId=user_id).filter_by(direction='send').group_by(phoneNumber).order_by(db.desc("sent_count"))
     print "after first query"
     '''all_sent_contacts = all_sent_contacts.column_descriptions
